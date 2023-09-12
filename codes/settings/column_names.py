2becbec868c744767704d25c6a33669b2986ffee
Lily <marixko@gmail.com>
2023-09-12 12:37:35 +0200

    implementation homogenize_tables

---
+++ codes/settings/column_names.py
@@ -0,0 +1,40 @@
+TIME_SERIES_COLUMNS = [
+    'i:aimage',
+    'i:aimagerat',
+    'i:bimage',
+    'i:bimagerat',
+    'i:candid',
+    'i:chinr',
+    'i:chipsf',
+    'i:classtar',
+    'i:dec',
+    'i:fid',
+    'i:fwhm',
+    'i:isdiffpos',
+    'i:jd',
+    'i:maggaia',
+    'i:maggaiabright',
+    'i:magpsf',
+    'i:neargaia',
+    'i:neargaiabright',
+    'i:ra',
+    'i:sigmapsf',
+    'v:classification',
+    'd:mangrove_2MASS_name',
+    'd:mangrove_HyperLEDA_name',
+    'd:mangrove_ang_dist',
+    'd:mangrove_lum_dist',
+]
+
+STAMP_COLUMNS = [
+    'b:cutoutScience_stampData',
+    'b:cutoutTemplate_stampData',
+    'b:cutoutDifference_stampData',
+]
+
+REDUCE_COLUMNS = [
+    'd:mangrove_2MASS_name',
+    'd:mangrove_HyperLEDA_name',
+    'd:mangrove_ang_dist',
+    'd:mangrove_lum_dist',
+]
